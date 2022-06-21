56acd7691e314223c275351b9bb8512b159ca368
glynk <glyn_knight@hotmail.com>
2022-06-21 14:21:21 +0100

    Working on models and routes for date and topic query

--- models/models.js
+++ models/models.js
@@ -7,3 +7,20 @@ export async function getData(){
     return res.rows;
     
     }
+
+    export async function getByDate(){
+        const res = await query('SELECT * FROM getanote WHERE date = "20/06/2022" ')
+        console.log(res.rows);
+        
+        return res.rows;
+        
+        }
+
+
+ export async function getByTopic(){
+        const res = await query('SELECT * FROM getanote WHERE topic LIKE  %Jest% ')
+        console.log(res.rows);
+            
+        return res.rows;
+            
+            }
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import {getData} from "../models/models.js" 
+import {getData,
+        getByDate,
+        getByTopic
+} from "../models/models.js" 
 
-//GET REQUEST AlL USER
+//GET REQUEST ALL DATA
 router.get("/", async function (req, res) {
     const allData = await getData();
     let responseObj = {
@@ -15,5 +18,23 @@ router.get("/", async function (req, res) {
 
 });
 
+/*
+//GET BY DATE
+router.get("/" , async function (req, res){
+    const data = await getByDate();
+    let responceObj = {
+        sucess: true,
+        data: data
+    }
+})
+
+router.get("/:topics" , async function (req, res){
+    const data = await getByTopic(req.query.topics);
+    let responceObj = {
+        sucess: true,
+        data: data
+    }
+})
+*/
 export default router;
 
