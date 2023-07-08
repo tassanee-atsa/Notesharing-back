9bc8fac97f50ed80ff8ce7881c03bc1d05ccb5aa
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-07-08 15:10:56 +0100

    move db to vercel

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@ import pg from "pg";
 //using the credentials from .env file
 //host db with Render
 const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
   ssl: { rejectUnauthorized: false },
 });
 
